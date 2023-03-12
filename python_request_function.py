e4a2afa2a9b31a77dc227ca924040d6819e48d19
amar13617 <amar13617@gmail.com>
2023-03-12 22:10:26 +0530

    first unittest commit

--- python_request_function.py
+++ python_request_function.py
@@ -1,1 +1,13 @@
 import requests
+import unittest
+def first_req_fun():
+    r = requests.get('https://httpbin.org/basic-auth/user/pass',auth=('user', 'pass'))
+    return r.json()
+print(first_req_fun())
+class TestString(unittest.TestCase):
+    
+    def test_request_function(self):
+        self.assertEqual(first_req_fun(), {'authenticated': True, 'user': 'user'})
+
+if __name__ == '__main__':
+    unittest.main()
