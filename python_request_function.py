cb3a4302e1a76083a66a46f5430324d5c2fe46d4
amar13617 <amar13617@gmail.com>
2023-03-13 11:06:43 +0530

    Third testcase commited

--- python_request_function.py
+++ python_request_function.py
@@ -2,20 +2,24 @@ import requests
 import unittest
 def first_req_fun(user):
     r = requests.get('https://httpbin.org/basic-auth/user/pass',auth=(user, 'pass'))
-    #return r.json()
-    if r.status_code == 200:
+    #return r.json()#1
+    #if r.status_code == 200:#2
+    #    return r.json()
+    #return " "
+    if r.status_code == 400:
         return r.json()
-    return " "
-    
+    return {'authenticated': True, 'user': 'user'}
 print(first_req_fun("amar"))
 class TestString(unittest.TestCase):
     
     #def test_request_function(self):
     #    self.assertEqual(first_req_fun(),{'authenticated': True, 'user': 'user'})
 
-    def test_request_function2(self):
-        self.assertNotEqual(first_req_fun("amar"),{'authenticated': True, 'user': 'user'})
+    #def test_request_function2(self):
+    #    self.assertNotEqual(first_req_fun("amar"),{'authenticated': True, 'user': 'user'})
 
+    def test_request_function3(self):
+        self.assertEqual(first_req_fun("amar"),{'authenticated': True, 'user': 'user'})
 
 if __name__ == '__main__':
     unittest.main()
