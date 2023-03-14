266bca446b949e2513cef87ce30ba8fe17e49c09
amar13617 <amar13617@gmail.com>
2023-03-14 08:55:10 +0530

    first testcase committed

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 import requests
-r = requests.get('https://httpbin.org/basic-auth/user/pass',auth=('user', 'pass'))
-print(r.text)
+import unittest
+def request_func(user):
+    r = requests.get('https://httpbin.org/basic-auth/user/pass',auth=('user', 'pass'))
+    return user
+
+print(request_func(20))
+
+class teststring(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual(request_func(20), 20)
+    
+
+if __name__ == '__main__':
+    unittest.main()
