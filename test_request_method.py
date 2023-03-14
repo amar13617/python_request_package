537904c75f5f1822352ddd46f8452b0cdacf2577
amar13617 <amar13617@gmail.com>
2023-03-14 22:00:39 +0530

    first testcases committed

--- test_request_method.py
+++ test_request_method.py
@@ -1,29 +1,13 @@
 import requests
 import unittest
-def request_func(user):
-    r = requests.get('https://httpbin.org/basic-auth/user/pass',auth=(user, 'pass'))
-    #return data #1
-    return user
-
-print(request_func("amar"))
+r = requests.get('https://httpbin.org/basic-auth/amar/kumar',auth=('amar', 'kumar'))
+#r1 = requests.get('https://www.w3schools.com/python/-auth/amar/kumar',auth=('amar', 'kumar'))
+print(r.text)
 
 class TestIntStringMethod(unittest.TestCase):
-
-    #def test_case_1(self):#1
-    #    self.assertEqual(request_func(20), 20)
-
-    #def test_case_2(self):#2 here user is what.
-    #    self.assertEqual(request_func(20), 20)
-
-    #def test_case_3(self):#3
-    #    self.assertEqual(request_func("amar"), "amar")
-
-
-
-
-
-
-    
-
+    def test_case1(self):
+        self.assertEqual({'authenticated': True, 'user': 'amar'},{'authenticated': True, 'user': 'amar'})
+    def test_case2(self):
+        self.assertEqual({"authenticated": True,"user": "amar"},{"authenticated": True,"user": "amar"})
 if __name__ == '__main__':
     unittest.main()
