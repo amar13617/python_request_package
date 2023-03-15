3750c3e553d5e63683c1d61d82a685ca45d397c7
amar13617 <amar13617@gmail.com>
2023-03-15 20:21:25 +0530

    second post testcases committed

--- python_post_request_method.py
+++ python_post_request_method.py
@@ -1,16 +1,23 @@
 import requests
 import unittest
 def request_func():
-    resp = requests.post('https://httpbin.org/post', data={'website': 'datagy.io'})
+    resp = requests.post('https://httpbin.org/post', data={'data1': 'value1', 'data2': 'value2'})
     #return resp.status_code
-    return resp.ok
+    #return resp.ok #1
+    #return resp.text #2
+
 
 print(request_func())
 
 class testpost(unittest.TestCase):
 
-    def test_case1(self):
-        self.assertEqual(request_func(), True)
+    #def test_case1(self):
+    #    self.assertEqual(request_func(), True)#1
+
+    #def test_case2(self):#doubt
+    #    self.assertEqual(request_func(),'{\n  "args": {}, \n  "data": "", \n  "fi[485 chars]n}\n')
+
+    
 
 if __name__ == '__main__':
     unittest.main()
