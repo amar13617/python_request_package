132abc074028b4f440c8a11c0cb12d93c34429f2
amar13617 <amar13617@gmail.com>
2023-03-15 20:30:01 +0530

    third post testcase committed

--- python_post_request_method.py
+++ python_post_request_method.py
@@ -5,7 +5,7 @@ def request_func():
     #return resp.status_code
     #return resp.ok #1
     #return resp.text #2
-
+    return resp.encoding
 
 print(request_func())
 
@@ -17,7 +17,9 @@ class testpost(unittest.TestCase):
     #def test_case2(self):#doubt
     #    self.assertEqual(request_func(),'{\n  "args": {}, \n  "data": "", \n  "fi[485 chars]n}\n')
 
-    
+    #def test_case2(self):
+    #    self.assertEqual(request_func(),'utf-8')
+
 
 if __name__ == '__main__':
     unittest.main()
