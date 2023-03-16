9137f40fc0dc31ae4990d9079eebd702d9d25f5c
amar13617 <amar13617@gmail.com>
2023-03-16 09:03:49 +0530

    fifth post testcase committed

--- python_post_request_method.py
+++ python_post_request_method.py
@@ -1,12 +1,15 @@
 import requests
 import unittest
 def request_func():
-    resp = requests.post('https://httpbin.org/post', data={'data1': 'value1', 'data2': 'value2'})
+    resp = requests.post('https://httpbin.org/post', data={ 'username': 'user@email.com', 'password': 'blahblahsecretpassw0rd' })
     #return resp.status_code
     #return resp.ok #1
     #return resp.text #2
-    #return resp.encoding
-    #return resp.headers['content-type']
+    #return resp.encoding#3
+    #return resp.headers['content-type']#4
+    if resp.status_code == 200:
+        return resp.encoding
+    return ""
 
 print(request_func())
 
@@ -18,8 +21,8 @@ class testpost(unittest.TestCase):
     #def test_case2(self):#doubt
     #    self.assertEqual(request_func(),'{\n  "args": {}, \n  "data": "", \n  "fi[485 chars]n}\n')
 
-    #def test_case3(self):
-    #    self.assertEqual(request_func(),'utf-8')
+    def test_case3(self):
+        self.assertEqual(request_func(),'utf-8')
 
     #def test_case4(self):
     #    self.assertEqual(request_func(),'application/json')
